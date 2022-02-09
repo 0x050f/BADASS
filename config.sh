3d9f65605a5403afb6609d83a10a3a03e56f98ff
Yellow <47715948+y3ll0w42@users.noreply.github.com>
2022-02-09 12:32:34 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get -y update && sudo apt-get -y upgrade
 
-sudo apt-get install python3-pip
+# GNS3
+sudo apt-get -y install python3-pip
 pip3 install gns3-gui gns3-server
+
+# Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
